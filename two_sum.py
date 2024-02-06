23c3d7e80570608b077eb10098df2532132cd638
melihcalis <mel11h@yandex.com>
2024-02-06 09:48:10 +0300

    updated two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,11 +1,21 @@
-class Solution:
+"""class Solution:
     def twoSum(self, nums, target):
         values_indexes = {}
         for i in range(len(nums)):
             if target-nums[i] in values_indexes:
                 return [i, values_indexes[target-nums[i]]]
             else:
-                values_indexes[nums[i]] = i
+                values_indexes[nums[i]] = i"""
+
+class Solution:
+    def twoSum(self, nums, target):
+        values_indexes = {}
+        for i, n in enumerate(nums):
+            if target-n in values_indexes:
+                return [i, values_indexes[target-n]]
+            else:
+                values_indexes[n] = i
+    
 
 sol = Solution()
 print(sol.twoSum([2,7,11,15], 9))
